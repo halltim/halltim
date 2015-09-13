bdcf93cd20651d4772a5951a1f650624e5aa5228
Tim Hall <timhall3000@gmail.com>
2015-09-13 13:14:27 -0700

    Added mailer

---
+++ app/mailers/contact_me.rb
@@ -0,0 +1,11 @@
+class ContactMe < ApplicationMailer
+  def contact_email (name, email, phone, message)
+    @name = name
+    @email = email
+    @phone = phone
+    @message = message
+    @recipient_email = timhall3000@gmail.com
+    @subject = "Website Contact Form:  #{@name}"
+    mail(to: @recipient_email, subject: @subject)
+  end
+end
