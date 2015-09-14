e8eb14abf4870c5070942f54a83409f087ec6375
Tim Hall <timhall3000@gmail.com>
2015-09-13 17:08:14 -0700

    Added From email default

--- app/mailers/contact_me.rb
+++ app/mailers/contact_me.rb
@@ -1,4 +1,5 @@
 class ContactMe < ApplicationMailer
+  default from: "contact@halltim.com"
   def contact_email (name, email, phone, message)
     @name = name
     @email = email
@@ -6,6 +7,7 @@ class ContactMe < ApplicationMailer
     @message = message
     @recipient_email = "timhall3000@gmail.com"
     @subject = "Website Contact Form:  #{@name}"
+    @sender_email = "contact@halltim.com"
     mail(to: @recipient_email, subject: @subject)
   end
 end
