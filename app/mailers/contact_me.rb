95db53c3b740a5c5b504bd88d0aeaf8cfcedebc5
Tim Hall <timhall3000@gmail.com>
2015-09-13 18:36:04 -0700

    Edited contact mailer.

--- app/mailers/contact_me.rb
+++ app/mailers/contact_me.rb
@@ -6,7 +6,7 @@ class ContactMe < ApplicationMailer
     @phone = phone
     @message = message
     @recipient_email = "timhall3000@gmail.com"
-    @subject = "HallTim.com Contact From #{@name}"
+    @subject = "Website Contact From #{@name}"
     mail(to: @recipient_email, subject: @subject, from: @email)
   end
 end
